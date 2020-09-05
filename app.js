e8b16415308ef1f9594c7f2cac2990cddcdf4320
isaac-glitchcode <isaac.su@gmx.com>
2020-09-04 19:18:21 -0500

    Function Middleware Login & register Added

--- app.js
+++ app.js
@@ -1,40 +1,91 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+//__dirname es una variable global dentro del contexto de node que nos regresa la ruta absoluta donde nos ubicamos actualmente
+//path.join nos permite construir rutas absolutas
+//express.static nos sirve para indicar que nuestro servidor va estar sirviendo archivos estaticos sobre el directorio public
+
 
 
 app.listen(8000, () => {console.log("Server Up!")});
 
-app.use('/', express.static('assets'))
 
-app.get('/inicio',(request,response) => {
-    response.sendFile('assets/login.html', {root: __dirname}) 
+app.use('/', express.static('assets'))
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/',(request,response) => {
+    response.sendFile(path.join(__dirname,'assets/login.html')) ;
 })
+
+app.post('/login',(request,response) => {
+    let email = request.body.email;
+    let password = request.body.password;
+    
+    fs.readFile('db.json', (error, data) => {
+        if(error){console.log(error)}
+
+        let users = JSON.parse(data.toString());
+        
+        let res = users.find(user =>{
+            return (user.email === email && user.password === password)? true:false;  
+        })
+        
+        res ? response.redirect('/_index.html') : response.redirect("/");
+
+    })
+})
+
+app.post('/register', (request, response) => {
+    console.log(request)
+    fs.readFile('db.json', (error, data) => {
+        if(error){console.log(error)}
+        //Transformar la data a un obj js para su manupulación
+        //Se parcea a JSON la data convertida a string
+        let users = JSON.parse(data.toString());
+        //Agrego a mi JSON los datos recibidos a través de la petición
+        users.push(request.body);
+        //Convierto a String los datos optenidos y los agrego al db.json para luego redireccionar a la raíz
+        fs.writeFile('db.json', JSON.stringify(users), (error) => {
+            if(error){console.log(error)}
+
+            response.redirect('/');
+        }) 
+    })
+
+})
+
+
+
 app.get('/registro',(request,response) => {
-    response.sendFile('assets/register.html', {root: __dirname})
+    response.sendFile(path.join(__dirname, 'assets/register.html'))
 })
 app.get('/restablecer-contrasena',(request,response) => {
-    response.sendFile('assets/forgot-password.html', {root: __dirname})
+    response.sendFile(path.join(__dirname,'assets/forgot-password.html'))
 })
 app.get('/nosotros',(request,response) => {
-    if(request.url==="/nosotros"){
         
-        fs.readFile('counter.txt',(error, data)=>{
-            let visits = data.toString().split(':')[1];
-            
-            visits++
+    fs.readFile('counter.txt',(error, data)=>{
+        let visits = data.toString().split(':')[1];
+        
+        visits++
 
-            fs.writeFile('counter.txt', `visits:${visits}`,(err)=>{
-                if(err){console.log(err)}
-            })
+        fs.writeFile('counter.txt', `visits:${visits}`,(err)=>{
+            if(err){console.log(err)}
+        })
 
-            console.log(visits)
-            
-            response.send('<h1>Visitas: '+visits+'</h1>')
-        }) 
-    }
+        console.log(visits)
+        
+        response.send('<h1>Visitas: '+visits+'</h1>')
+    })
 })
 app.get('*', (request, response)=>{
-    request.url==='/'? response.sendFile('assets/login.html', {root:__dirname})
-    :response.sendFile('assets/404.html', {root:__dirname})
+    request.url==='/' ? response.sendFile(path.join(__dirname,'assets/login.html'))
+    : response.sendFile(path.join(__dirname,'assets/404.html'))
+    
 })
+
