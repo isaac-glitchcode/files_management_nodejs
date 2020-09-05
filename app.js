e9c21a2753a8d2d89c6b86b32c6c5949aef0b570
isaac-glitchcode <54855963+isaac-glitchcode@users.noreply.github.com>
2020-09-04 19:59:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 18965
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {console.log("Server Up!")});
 console.log("Puerto: "+PORT);
 app.use('/', express.static('assets'))
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get('/',(request,response) => {
